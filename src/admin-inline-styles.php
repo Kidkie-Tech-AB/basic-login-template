e8c4102224cd088f084117a8952733ab230d117f
Martin Ekelund <martin@kidkie.se>
2025-01-23 22:56:02 +0100

    do not use styles on interim login

--- src/admin-inline-styles.php
+++ src/admin-inline-styles.php
@@ -14,29 +14,29 @@ function blt_custom_login_css() {
         --bg-color: <?php echo $bg_color; ?>;
         --text-color: <?php echo $text_color; ?>;
     }
-    body.login div#login::before {
+    body.login:not(.interim-login) div#login::before {
         background-color: var(--text-color);       
     }
 
-    body.login form#loginform .submit input[type="submit"] {
+    body.login:not(.interim-login) form#loginform .submit input[type="submit"] {
         color: var(--bg-color, #FFFFFF) !important;
         border-color: var(--text-color, #0073aa);
         background-color: var(--text-color, #0073aa) !important;
     }
-    body.login form#loginform .forgetmenot label {
+    body.login:not(.interim-login) form#loginform .forgetmenot label {
         color: var(--text-color, #0073aa) !important;
     }
-    body.login #backtoblog a, .login #nav a {
+    body.login:not(.interim-login) #backtoblog a, .login #nav a {
         color: var(--text-color, #0073aa) !important;
     }
-    body.login .notice.notice-info.message {
+    body.login:not(.interim-login) .notice.notice-info.message {
         color: var(--text-color, #0073aa);
         border-color: var(--text-color, #0073aa);
     }
-    body.login .notice.notice-info.message::after {
+    body.login:not(.interim-login) .notice.notice-info.message::after {
         background-color: var(--text-color, #FFFFFF) !important;
     }
-    body.login.wp-core-ui .button.button-large {
+    body.login.wp-core-ui:not(.interim-login) .button.button-large {
         color: var(--bg-color, #0073aa);
         border-color: var(--text-color, #0073aa);
         background-color: var(--text-color, #0073aa) !important;
