f2373e98dc9c362787c52502e8015ee8ae9cea08
Martin Ekelund <martin@kidkie.se>
2025-01-26 13:49:27 +0100

    make background pick up colors as well

--- src/admin-inline-styles.php
+++ src/admin-inline-styles.php
@@ -14,10 +14,15 @@ function blt_custom_login_css() {
         --bg-color: <?php echo $bg_color; ?>;
         --text-color: <?php echo $text_color; ?>;
     }
+    body.login:not(.interim-login) {
+        background: var(--text-color, rgb(238, 174, 202));
+        background: -moz-radial-gradient(circle, rgba(238, 174, 202, 1) 0%, var(--text-color, rgba(148, 187, 233, 1)) 100%);
+        background: -webkit-radial-gradient(circle, rgba(238, 174, 202, 1) 0%, var(--text-color, rgba(148, 187, 233, 1)) 100%);
+        background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, var(--text-color, rgba(148, 187, 233, 1)) 100%);
+    }
     body.login:not(.interim-login) div#login::before {
         background-color: var(--text-color);       
     }
-
     body.login:not(.interim-login) form#loginform .submit input[type="submit"] {
         color: var(--bg-color, #FFFFFF) !important;
         border-color: var(--text-color, #0073aa);
@@ -40,6 +45,9 @@ function blt_custom_login_css() {
         color: var(--bg-color, #0073aa);
         border-color: var(--text-color, #0073aa);
         background-color: var(--text-color, #0073aa) !important;
+    }
+    body.login:not(.interim-login) a, body.login:not(.interim-login) a:hover {
+        color: var(--text-color, #0073aa) !important;
     }
     </style>
 <?php
